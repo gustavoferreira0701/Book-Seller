7675c3a8615ccf4c79038425267eed190dfe1c56
Adriano Kretschmer <a.kretschmer@cinq.com.br>
2017-10-13 09:15:12 -0300

    Initial version

---
+++ BooksSeller.WebApi/Providers/BooksProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksSeller.WebApi.Providers
+{
+    public class BooksProvider //: IBooksProvider
+    {
+        // TODO: Create the implementation for IBooksProvider
+    }
+}
